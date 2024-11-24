630b62e07fb70136c0c71425dad02bd75c8dfcfb
valer <valer@DESKTOP-QD94NVR>
2024-11-24 18:11:16 +0300

    Добавьте файлы проекта.

---
+++ Lab3_7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер члена ряда Фибоначчи (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Получение n-ого члена ряда Фибоначчи
+        int fibonacciNumber = Fibonacci(n);
+        Console.WriteLine($"Член ряда Фибоначчи с номером {n} равен: {fibonacciNumber}");
+    }
+
+    // Рекурсивная функция для нахождения n-ого члена ряда Фибоначчи
+    static int Fibonacci(int n)
+    {
+        if (n <= 0)
+            return 0; // Для n = 0 возвращаем 0
+        else if (n == 1)
+            return 1; // Для n = 1 возвращаем 1
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2); // Рекурсивный вызов
+    }
+}
